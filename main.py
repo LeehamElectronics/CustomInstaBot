09325767d221a20a2bb66838ba528d35cdd5cc8c
Liam Price <ldprice@ad.ldprice.home>
2023-06-13 11:38:42 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+####################
+# custom insta bot #
+####################
+import json
+import re
+import requests
+
+
+def login_insta(account):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Logging into, {account}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    login_insta('account')
